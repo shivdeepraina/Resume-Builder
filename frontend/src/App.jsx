53f7170cf2d71b1c79a33d18fec35eb33f22fb46
bera meet <130121303+BERAMEET7@users.noreply.github.com>
2024-07-12 16:39:09 -0400

    first page in react created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import FirstPage from './Firstpage';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <FirstPage />
+    </div>
+  );
+}
+
+export default App;
+
